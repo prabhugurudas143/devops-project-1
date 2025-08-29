546b312c737f8e44ea104f0b879de35abfb7624d
prabhugurudas143 <prabhugurudas143@gmail.com>
2025-08-30 02:35:04 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -2,7 +2,8 @@ provider "aws" {
   #region                   = "eu-central-1"
   # shared_credentials_files = ["/Users/rahulwagh/.aws/credentials"]
 
-  profile = "tf-user"
+  #profile = "tf-user"
   region = "eu-central-1"
+
 
 }
